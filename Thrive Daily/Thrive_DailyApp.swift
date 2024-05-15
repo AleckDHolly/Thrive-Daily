e3a1ff8fb24b7d4999d685ec00483879a6937e70
Aleck David Holly <81864805+AleckDHolly@users.noreply.github.com>
2024-05-15 19:08:14 -0400

    Initial Commit

---
+++ Thrive Daily/Thrive_DailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thrive_DailyApp.swift
+//  Thrive Daily
+//
+//  Created by Aleck Holly on 2024-05-15.
+//
+
+import SwiftUI
+
+@main
+struct Thrive_DailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
